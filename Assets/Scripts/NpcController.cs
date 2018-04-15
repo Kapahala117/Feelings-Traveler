c64ec0bbc4ee37c0b891e574d0c7fdb08ab15832
Kapahala <i.jimenez.u33@gmail.com>
2018-04-15 18:47:54 +0200

    Player movement, Npcs, UI, NavMesh

---
+++ Assets/Scripts/NpcController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NpcController : MonoBehaviour {
+
+    GameObject player;
+
+	// Use this for initialization
+	void Start () {
+        player = GameObject.FindGameObjectWithTag("Player");
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (Vector3.Distance(player.transform.position,this.transform.position)<10.0f) { 
+                    if (hit.transform.gameObject.tag == "Npc")
+                    {
+                        Debug.Log("HOLIS");
+                        //HABLAAAARRR
+                    }
+                }
+            }
+        }
+		
+	}
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerMovement : MonoBehaviour {
+
+    NavMeshAgent agent;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        agent.enabled = true;
+
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("He hecho click, JOPUTA");
+            RaycastHit hit;
+
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 1000))
+            {
+                agent.destination = hit.point;
+                Debug.Log("Tengo que moverme, joputilla");
+                
+            }
+        }
+    }
+}
