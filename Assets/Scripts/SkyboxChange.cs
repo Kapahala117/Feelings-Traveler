aa4f83f2a1f0331d0596747b79d231b3380552f4
Kapahala <i.jimenez.u33@gmail.com>
2018-06-20 19:43:43 +0200

    Update

---
+++ Assets/Scripts/SkyboxChange.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkyboxChange : MonoBehaviour {
+
+    public Material enterSkybox;
+    public Material exitSkybox;
+    // Use this for initialization
+    void Start () {
+       
+    }
+	
+	// Update is called once per frame
+	void Update () {
+       
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+            RenderSettings.skybox = enterSkybox;
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+            RenderSettings.skybox = exitSkybox;
+    }
+}
